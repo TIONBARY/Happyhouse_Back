19f8e15daca829ee2e665e063972d6e0217b1242
nadad <nadad@DESKTOP-FMTBGUO>
2022-03-30 01:23:38 +0900

    commit

---
+++ WebContent/js/my.js
@@ -0,0 +1,90 @@
+$(document).ready(function () {
+	const name = $.cookie("name");
+	if (name) {
+		$("#loginSpan").html(name + "<button id ='logoutBtn'>logout</button>");
+		$("#userinfo").click(function(){
+			$.post("main",{name, sign:"userinfo"},function(data){
+			})
+		})
+	} 
+
+	$(document).on("click", "#logoutBtn", function () {
+		$.post("main", { sign: "logout" }, function () {
+			$.removeCookie("name");
+			location.reload();
+		});
+	});
+
+	$("#loginBtn").click(function(){
+		const id = $("#id").val();
+		const pw = $("#pw").val();
+		
+		$.post("main",{id,pw,sign:"login"},function(data){
+			data=JSON.parse(data);
+			console.log(data);
+			
+			if(data.msg){
+				alert(data.msg);
+			}else if(data.name){
+				$.cookie("name",data.name);
+				location.href = "index.jsp";
+			}
+		});
+	});
+	
+	$("#findPw").click(function () {
+		let id = $("#id").val();
+		let user = JSON.parse(localStorage.getItem(id));
+
+		alert("비밀번호는 " + user.password + "입니다.");
+	});
+
+	$("#registBtn").click(function () {
+		let id = $("#id").val();
+		let password = $("#password").val();
+		let name = $("#name").val();
+		let email = $("#email").val();
+		let age = $("#age").val();
+		
+		$.post("main", { id, password, name, email, age, sign: "regist" }, function (data) {
+			console.log(data);
+			data = JSON.parse(data);
+			console.log(data);
+			if(data.msg){
+				alert(data.msg);
+			}
+			location.href = "index.jsp";
+		});
+	});
+
+	$("#user_info_button_ok").click(function () {
+		location.href = "index.jsp";
+	});
+
+	$("#user_info_button_edit").click(function () {
+		let id = $("#user_info_id").val();
+		let password = $("#user_info_password").val();
+		let name = $("#user_info_name").val();
+		let email = $("#user_info_email").val();
+		let age = $("#user_info_age").val();
+
+		const user = {
+				id: id,
+				password: password,
+				name: name,
+				email: email,
+				age: age,
+		};
+
+		localStorage.setItem(id, JSON.stringify(user));
+		alert("회원정보 수정");
+		location.href = "index.jsp";
+	});
+	
+	$("#user_info_button_remove").click(function () {
+		$.removeCookie("id");
+		
+		alert(user_info.id + "님 회원탈퇴하셨습니다.");
+		location.href = "index.jsp";
+	});
+});
---
+++ src/web/controller/mainServlet.java
@@ -0,0 +1,87 @@
+package web.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.json.JSONObject;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import web.dao.UserDAO;
+import web.util.MyException;
+import web.vo.UserVO;
+
+public class mainServlet extends HttpServlet {
+	HashMap<String, Controller> map;
+	Controller ctrl;
+	
+	@Override
+	public void init() throws ServletException {
+		ServletContext application = getServletContext();
+		map = (HashMap<String, Controller>) application.getAttribute("map");
+		if(map == null) {
+			map = new HashMap<>();
+			application.setAttribute("map", map);
+		}
+		
+		try {
+			SAXParserFactory f = SAXParserFactory.newInstance();
+			SAXParser parser = f.newSAXParser();
+			parser.parse("C:\\SSAFY\\happyproject\\0310_HaapyHouse\\action-servlet.xml", new DefaultHandler() {
+				@Override
+				public void startElement(String uri, String localName, String qName, Attributes attributes)
+						throws SAXException {
+					try {
+						if(qName.equals("bean")) {
+							String id = attributes.getValue("id");
+							String clazz = attributes.getValue("class");
+							Class clazz2 = Class.forName(clazz);
+							Controller value = (Controller)clazz2.newInstance();
+							map.put(id, value);
+						}
+					}catch(Exception e) {
+						e.printStackTrace();
+					}
+				} 
+			});
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+
+		String sign = request.getParameter("sign");
+		if (sign != null) {
+			try {
+				map.get(sign).execute(request, response);
+			} catch (MyException e) {
+				JSONObject jo = new JSONObject();
+				jo.put("msg", e.getMessage());
+				out.append(jo.toString());
+			}
+		} else {
+			request.getRequestDispatcher("/index.jsp").forward(request, response);
+		}
+	}
+}
